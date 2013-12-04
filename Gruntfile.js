e835f49b8165e3c405eedbd38bb9a3e082f789f5
Dorian Kernytsky <DorianK@gmail.com>
2013-12-04 14:44:55 -0500

    reformated code and added comments

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,27 +1,30 @@
-module.exports = function(grunt){
+module.exports = function(grunt) {
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
-    watch:{
-      options: {
-        livereload: true,
-      },
-        css: {
-          files: ['sass/**/*.scss'],
-          tasks: ['compass']
-        }
-    },
-  compass: {
-    dist: {
-      options: {
-        config: 'config.rb'
-      }
-    }
-  }
-  });
-  grunt.loadNpmTasks('grunt-contrib-compass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-
-  grunt.registerTask('default', ['watch']);
-
+	grunt.initConfig({
+		//make sure all the packages are install …I beliave
+		pkg: grunt.file.readJSON('package.json')
+		watch: {
+			//run livereload server
+			options: {
+				livereload: true,
+			},
+			//watch the folder and tell which task to run
+			css: {
+				files: ['sass/**/*.scss'],
+				tasks: ['compass']
+			}
+		},
+		//tell Compass to use config.rb
+		compass: {
+			dist: {
+				options: {
+					config: 'config.rb'
+				}
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	//setup default task
+	grunt.registerTask('default', ['watch']);
 };
