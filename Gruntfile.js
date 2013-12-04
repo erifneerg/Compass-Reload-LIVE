78467ee3aebec5c229794e1bb45cac60d5541e89
Dorian Kernytsky <DorianK@gmail.com>
2013-12-04 14:07:44 -0500

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function(grunt){
+
+  grunt.initConfig({
+    //pkg: grunt.file.readJSON('package.json')
+    watch:{
+      options: {
+        livereload: true,
+      },
+        css: {
+          files: ['sass/**/*.scss'],
+          tasks: ['compass']
+        }
+    },
+  compass: {
+    dist: {
+      options: {
+        config: 'config.rb'
+      }
+    }
+  }
+  });
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  //  grunt.registerTask('default', []);
+
+};
